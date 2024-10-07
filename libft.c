ec50e6cd8153edb2089e4828a5ae9867efca3960
Tim Hessenauer <thessena@1-C-32.42heilbronn.de>
2024-10-07 17:12:51 +0200

    ft_strlen erstellt

--- libft.c
+++ libft.c
@@ -6,12 +6,26 @@
 /*   By: thessena <thessena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 16:38:06 by thessena          #+#    #+#             */
-/*   Updated: 2024/10/07 16:43:15 by thessena         ###   ########.fr       */
+/*   Updated: 2024/10/07 17:12:24 by thessena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(void)
+#include <stddef.h>
+#include <stdio.h>
+
+int	ft_strlen(const char *str)
 {
-    
-    return 0;
+	size_t i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+int	main(void)
+{
+	int len;
+	len = ft_strlen("asdf");
+	printf("%i\n", len);
 }

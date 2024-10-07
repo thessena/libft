50efb04028f3f5241722989abec570d0d1f8f242
Tim Hessenauer <thessena@1-C-32.42heilbronn.de>
2024-10-07 17:36:55 +0200

    added isalpha

--- libft.c
+++ libft.c
@@ -6,7 +6,7 @@
 /*   By: thessena <thessena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 16:38:06 by thessena          #+#    #+#             */
-/*   Updated: 2024/10/07 17:12:24 by thessena         ###   ########.fr       */
+/*   Updated: 2024/10/07 17:35:49 by thessena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 int	ft_strlen(const char *str)
 {
-	size_t i;
+	size_t	i;
 
 	i = 0;
 	while (str[i] != '\0')
@@ -23,9 +23,27 @@ int	ft_strlen(const char *str)
 	return (i);
 }
 
+int	ft_isalpha(int c)
+{
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+		return (c);
+	return (0);
+}
+
 int	main(void)
 {
-	int len;
+	int		len;
+	char	c1;
+	char	c2;
+	char	c3;
+
 	len = ft_strlen("asdf");
+	c1 = 'A';
+	c2 = 'g';
+	c3 = '5';
 	printf("%i\n", len);
+	printf("%c %d\n", c1, ft_isalpha(c1));
+	printf("%c %d\n", c2, ft_isalpha(c2));
+	printf("%c %d\n", c3, ft_isalpha(c3));
+	return (0);
 }

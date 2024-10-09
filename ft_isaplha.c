350fd574638542e91be351ad50d92f150f19834e
Tim Hessenauer <thessena@1-C-32.42heilbronn.de>
2024-10-09 14:47:28 +0200

    use multiple functions

--- ft_isaplha.c
+++ ft_isaplha.c
@@ -1,34 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.c                                            :+:      :+:    :+:   */
+/*   ft_isaplha.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: thessena <thessena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/07 16:38:06 by thessena          #+#    #+#             */
-/*   Updated: 2024/10/09 14:44:33 by thessena         ###   ########.fr       */
+/*   Created: 2024/10/09 14:38:19 by thessena          #+#    #+#             */
+/*   Updated: 2024/10/09 14:42:32 by thessena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
-#include <stdio.h>
-#include "ft_strlen.c"
-#include "ft_isaplha.c"
-
-int	main(void)
+int	ft_isalpha(int c)
 {
-	int		len;
-	char	c1;
-	char	c2;
-	char	c3;
-
-	len = ft_strlen("asdf");
-	c1 = 'A';
-	c2 = 'g';
-	c3 = '5';
-	printf("%i\n", len);
-	printf("%c %d\n", c1, ft_isalpha(c1));
-	printf("%c %d\n", c2, ft_isalpha(c2));
-	printf("%c %d\n", c3, ft_isalpha(c3));
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+		return (c);
 	return (0);
 }
--- ft_strlen.c
+++ ft_strlen.c
@@ -1,34 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.c                                            :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: thessena <thessena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/07 16:38:06 by thessena          #+#    #+#             */
-/*   Updated: 2024/10/09 14:44:33 by thessena         ###   ########.fr       */
+/*   Created: 2024/10/09 14:37:11 by thessena          #+#    #+#             */
+/*   Updated: 2024/10/09 14:42:36 by thessena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
-#include <stdio.h>
-#include "ft_strlen.c"
-#include "ft_isaplha.c"
-
-int	main(void)
+int	ft_strlen(const char *str)
 {
-	int		len;
-	char	c1;
-	char	c2;
-	char	c3;
+	size_t	i;
 
-	len = ft_strlen("asdf");
-	c1 = 'A';
-	c2 = 'g';
-	c3 = '5';
-	printf("%i\n", len);
-	printf("%c %d\n", c1, ft_isalpha(c1));
-	printf("%c %d\n", c2, ft_isalpha(c2));
-	printf("%c %d\n", c3, ft_isalpha(c3));
-	return (0);
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
 }

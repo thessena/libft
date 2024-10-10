c1d9d33d84a881830e8567a2d90bce39ae365ddb
Tim Hessenauer <thessena@1-D-32.42heilbronn.de>
2024-10-10 15:00:03 +0200

    created ft_bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,28 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: thessena <thessena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/09 14:59:25 by thessena          #+#    #+#             */
-/*   Updated: 2024/10/10 14:58:31 by thessena         ###   ########.fr       */
+/*   Created: 2024/10/10 14:41:24 by thessena          #+#    #+#             */
+/*   Updated: 2024/10/10 14:58:19 by thessena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <unistd.h>
-# include <stdlib.h>
+void	ft_bzero(void *s, size_t n)
+{
+	char	*ptr;
 
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-int		ft_strlen(const char *s);
-void	*ft_memset(void *b, int c, size_t len);
-void	ft_bzero(void *s, size_t n);
-
-#endif
+	ptr = s;
+	while (n > 0)
+	{
+		*ptr = '\0';
+		ptr++;
+		n--;
+	}
+}
--- ft_memset.c
+++ ft_memset.c
@@ -6,25 +6,22 @@
 /*   By: thessena <thessena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 14:27:25 by thessena          #+#    #+#             */
-/*   Updated: 2024/10/10 14:39:28 by thessena         ###   ########.fr       */
+/*   Updated: 2024/10/10 14:58:16 by thessena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*memset(void *b, int c, size_t len)
+void	*ft_memset(void *b, int c, size_t len)
 {
 	unsigned char	*ptr;
-	unsigned char	value;
-	size_t				i;
 
-	ptr = (unsigned char *)b;
-	value = (unsigned char)c;
-	i = 0;
-	while (i < len)
+	ptr = b;
+	while (len > 0)
 	{
-		ptr[i] = value;
-		i++;
+		*ptr = c;
+		ptr++;
+		len--;
 	}
 	return (b);
 }

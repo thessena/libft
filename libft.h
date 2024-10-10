5ac8118d7b87ee2100e02a16a095f58e4f3af31e
Tim Hessenauer <thessena@1-D-32.42heilbronn.de>
2024-10-10 14:24:07 +0200

    created ft_isascii and ft_isprint

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: thessena <thessena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 14:59:25 by thessena          #+#    #+#             */
-/*   Updated: 2024/10/10 13:53:46 by thessena         ###   ########.fr       */
+/*   Updated: 2024/10/10 14:23:37 by thessena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
 int		ft_strlen(const char *s);
 
 #endif

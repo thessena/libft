37fb6030c16f4f8e06e6c6f127e8250450f5d4cb
thessena <timhessenauer@gmail.com>
2024-10-23 08:59:44 +0200

    deleted unneccessary functions and comments

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,58 +6,11 @@
 /*   By: thessena <thessena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 16:36:00 by thessena          #+#    #+#             */
-/*   Updated: 2024/10/17 10:48:49 by thessena         ###   ########.fr       */
+/*   Updated: 2024/10/23 08:57:40 by thessena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-/* int	ft_checksigns(char *str)
-{
-	if (*str == '-')
-	{
-		sign = -1;
-		has_sign = 1;
-		str++;
-	}
-	if (*str == '+')
-	{
-		if (has_sign)
-			return (0);
-		str++;
-	}
-} */
-
-/* int	ft_atoi(const char *str)
-{
-	int	sign;
-	int	result;
-	int	has_sign;
-
-	sign = 1;
-	result = 0;
-	has_sign = 0;
-	while (*str == ' ' || (*str >= 9 && *str <= 13))
-		str++;
-	if (*str == '-')
-	{
-		sign = -1;
-		has_sign = 1;
-		str++;
-	}
-	if (*str == '+')
-	{
-		if (has_sign)
-			return (0);
-		str++;
-	}
-	while (*str >= '0' && *str <= '9')
-	{
-		result = result * 10 + (*str - '0');
-		str++;
-	}
-	return (sign * result);
-} */
 
 int	ft_atoi(const char *str)
 {
@@ -83,29 +36,3 @@ int	ft_atoi(const char *str)
 	}
 	return (sign * result);
 }
-
-/* int	ft_atoi(const char *str)
-{
-	int	sign;
-	int	result;
-	int	i;
-
-	sign = 1;
-	result = 0;
-	i = 0;
-	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
-		i++;
-	if (str[i] == '-')
-	{
-		sign = -1;
-		i++;
-	}
-	if (str[i] == '+')
-		i++;
-	while (*str >= '0' && *str <= '9')
-	{
-		result = result * 10 + (*str - '0');
-		str++;
-	}
-	return (sign * result);
-} */

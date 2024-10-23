a5a50c598357fbf35b5d2fecb643680fba9fa090
thessena <timhessenauer@gmail.com>
2024-10-23 15:39:57 +0200

    created lstlast

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: thessena <thessena@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/10/09 16:31:53 by thessena          #+#    #+#              #
-#    Updated: 2024/10/23 15:32:10 by thessena         ###   ########.fr        #
+#    Updated: 2024/10/23 15:37:18 by thessena         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -61,7 +61,7 @@ BONUS_SOURCES = \
 	ft_lstnew.c \
 	ft_lstadd_front.c \
 	ft_lstsize.c \
-	# ft_lstlast.c \
+	ft_lstlast.c \
 	# ft_lstadd_back.c \
 	# ft_lstdelone.c \
 	# ft_lstclear.c \
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: thessena <thessena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 14:59:25 by thessena          #+#    #+#             */
-/*   Updated: 2024/10/23 15:32:17 by thessena         ###   ########.fr       */
+/*   Updated: 2024/10/23 15:37:22 by thessena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,7 +59,7 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
-// t_list	*ft_lstlast(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
 // void	ft_lstadd_back(t_list **lst, t_list *new);
 // void	ft_lstdelone(t_list *lst, void (*del)(void *));
 // void	ft_lstclear(t_list **lst, void (*del)(void *));

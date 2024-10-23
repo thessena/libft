6eecb9b0a3e82f6facf6dbd14cd3faa90a9842a3
thessena <timhessenauer@gmail.com>
2024-10-23 09:59:19 +0200

    improved split dramatically

--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,13 @@
 /*   By: thessena <thessena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 15:27:24 by thessena          #+#    #+#             */
-/*   Updated: 2024/10/18 13:28:22 by thessena         ###   ########.fr       */
+/*   Updated: 2024/10/23 09:56:04 by thessena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_count_words(char *s, char c)
+int	ft_count_words(const char *s, char c)
 {
 	int	count;
 	int	flag;
@@ -33,67 +33,39 @@ int	ft_count_words(char *s, char c)
 	return (count);
 }
 
-char	*ft_allocate_word(const char *s, char c)
+char	*ft_copy_word(const char *start, const char *end)
 {
-	int			len;
-	char		*word;
-	int			i;
+	char	*word;
+	int		i;
 
-	len = 0;
-	while (s[len] && s[len] != c)
-		len++;
-	word = (char *)malloc(len + 1);
+	word = (char *)malloc((end - start + 1) * sizeof(char));
 	if (!word)
 		return (NULL);
 	i = 0;
-	while (i < len)
-	{
-		word[i] = s[i];
-		i++;
-	}
-	word[len] = '\0';
+	while (start < end)
+		word[i++] = *start++;
+	word[i] = '\0';
 	return (word);
-}
-
-void	ft_free_all(char **result, int i)
-{
-	while (i > 0)
-		free(result[--i]);
-	free(result);
-}
-
-int	ft_assign_word(char **result, char const *s, char c, int i)
-{
-	result[i] = ft_allocate_word(s, c);
-	if (!result[i])
-	{
-		ft_free_all(result, i);
-		return (0);
-	}
-	return (1);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	char	**result;
-	int		i;
-	int		count;
+	char		**result;
+	int			i;
+	const char	*start;
 
-	if (!s)
-		return (NULL);
-	count = ft_count_words((char *)s, c);
-	result = (char **)malloc((count + 1) * sizeof(char *));
-	if (!result)
+	result = (char **)malloc((ft_count_words(s, c) + 1) * sizeof(char *));
+	if (!s || !(result))
 		return (NULL);
 	i = 0;
 	while (*s)
 	{
 		if (*s != c)
 		{
-			if (!ft_assign_word(result, s, c, i++))
-				return (NULL);
+			start = s;
 			while (*s && *s != c)
 				s++;
+			result[i++] = ft_copy_word(start, s);
 		}
 		else
 			s++;

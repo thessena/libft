bd77854e052a4814ecb6070291f97fa407833c88
thessena <timhessenauer@gmail.com>
2024-10-24 10:14:38 +0200

    fixed lstmap from paco -s errors

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: thessena <thessena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 15:54:07 by thessena          #+#    #+#             */
-/*   Updated: 2024/10/23 16:01:15 by thessena         ###   ########.fr       */
+/*   Updated: 2024/10/24 10:14:03 by thessena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,16 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
 	t_list	*new_node;
+	void	*new_content;
 
 	new_list = NULL;
 	while (lst != NULL)
 	{
-		new_node = ft_lstnew(f(lst->content));
+		new_content = f(lst->content);
+		new_node = ft_lstnew(new_content);
 		if (new_node == NULL)
 		{
+			del(new_content);
 			ft_lstclear(&new_list, del);
 			return (NULL);
 		}
